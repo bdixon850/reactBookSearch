e3cfc47f7427a5d87104ff372ada50b939b2a17b
Brandon Dixon <bdixon850@yahoo.com>
2019-04-25 15:50:20 -0400

    changed models

--- models/book.js
+++ models/book.js
@@ -2,22 +2,32 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title: {
-        type: String,
-        required: true
-    },
-    authors: {
-        type: String
-    },
-    description: {
-        type: String
-    },
+
+    title: String,
+    authors: [String],
+    description: String,
     img: {
-        type: String
+        type: {String}
     },
     link: {
         type: String
     }
+    // title: {
+    //     type: String,
+    //     // required: true
+    // },
+    // authors: {
+    //     type: String
+    // },
+    // description: {
+    //     type: String
+    // },
+    // img: {
+    //     type: String
+    // },
+    // link: {
+    //     type: String
+    // }
 });
 
 const Book = mongoose.model('Book', bookSchema);
--- routes/api-routes.js
+++ routes/api-routes.js
@@ -47,6 +47,6 @@ module.exports = function(app) {
     })
 
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, './client/build/index.html'))
+        res.sendFile(path.join(__dirname, '../client/build/index.html'))
     });
 }
